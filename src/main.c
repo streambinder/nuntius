a16c02fd922490a4b06d3896f47c1265fcce7f2e
streambinder <posta@davidepucci.it>
2019-05-20 17:46:20 +0200

    src: add effective data to notification

--- src/main.c
+++ src/main.c
@@ -79,99 +79,110 @@ int main(int argc, char *argv[])
         return 1;
     }
 
+    g_app = g_create("it.davidepucci.PaperBoy");
+    g_run(g_app);
+
     // printf("user %s password %s server %s\n", user_address, user_password, mail_server);
 
     struct sockaddr_in server;
     struct hostent *host;
     int sock;
 
-    sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock == -1)
-    {
-        printf("Unable to create socket: %d.", errno);
-        exit(EXIT_FAILURE);
-    }
-
-    server.sin_family = AF_INET;
-    server.sin_port = htons(143);
-    host = gethostbyname(mail_server);
-    if (host == NULL)
-    {
-        printf("Failed on gethostbyname: %s.", hstrerror(h_errno));
-        exit(EXIT_FAILURE);
-    }
-    memcpy(&server.sin_addr, host->h_addr_list[0], host->h_length);
-
-    if (connect(sock, (struct sockaddr *)&server, sizeof(server)) < 0)
-    {
-        printf("Failed on connecting to server socket.");
-        return -1;
-    }
-
-    char *message[512];
-    char *buffer[512];
     for (;;)
     {
+
+        sock = socket(AF_INET, SOCK_STREAM, 0);
+        if (sock == -1)
+        {
+            printf("Unable to create socket: %d.", errno);
+            exit(EXIT_FAILURE);
+        }
+
+        server.sin_family = AF_INET;
+        server.sin_port = htons(143);
+        host = gethostbyname(mail_server);
+        if (host == NULL)
+        {
+            printf("Failed on gethostbyname: %s.", hstrerror(h_errno));
+            exit(EXIT_FAILURE);
+        }
+        memcpy(&server.sin_addr, host->h_addr_list[0], host->h_length);
+
+        if (connect(sock, (struct sockaddr *)&server, sizeof(server)) < 0)
+        {
+            printf("Failed on connecting to server socket.");
+            return -1;
+        }
+
+        char *message[512];
+        char *buffer[512];
+        for (;;)
+        {
+            int packet_length = recv(sock, buffer, 512, 0);
+            buffer[packet_length] = '\0';
+            // printf("%s", buffer);
+            if (packet_length != 512)
+            {
+                break;
+            }
+        }
+        // printf("\n");
+
+        sprintf(message, "1 LOGIN %s %s\r\n", user_address, user_password);
+        if (write(sock, message, strlen(message)) < 0)
+        {
+            printf("Unable to send message.");
+            return -1;
+        }
+
+        for (;;)
+        {
+            int packet_length = recv(sock, buffer, 512, 0);
+            buffer[packet_length] = '\0';
+            // printf("%s", buffer);
+            if (packet_length != 512)
+            {
+                break;
+            }
+        }
+        // printf("\n");
+
+        sprintf(message, "1 STATUS INBOX (UNSEEN)\r\n");
+        if (write(sock, message, strlen(message)) < 0)
+        {
+            printf("Unable to send message.");
+            return -1;
+        }
+
         int packet_length = recv(sock, buffer, 512, 0);
         buffer[packet_length] = '\0';
-        // printf("%s", buffer);
-        if (packet_length != 512)
+
+        // printf("%s\n", buffer);
+
+        regex_t r;
+        const char *regex_text = ".*\\(UNSEEN ([[:digit:]]+)\\).*";
+        int status = regcomp(&r, regex_text, REG_EXTENDED | REG_NEWLINE);
+        if (status != 0)
         {
-            break;
+            printf("Regex error compiling '%s'\n", regex_text);
+            return 1;
         }
-    }
-    // printf("\n");
 
-    sprintf(message, "1 LOGIN %s %s\r\n", user_address, user_password);
-    if (write(sock, message, strlen(message)) < 0)
-    {
-        printf("Unable to send message.");
-        return -1;
-    }
+        const int unseen = atoi(first_match(&r, buffer));
 
-    for (;;)
-    {
-        int packet_length = recv(sock, buffer, 512, 0);
-        buffer[packet_length] = '\0';
-        // printf("%s", buffer);
-        if (packet_length != 512)
-        {
-            break;
-        }
-    }
-    // printf("\n");
+        printf("Unseen: %d\n", unseen);
 
-    sprintf(message, "1 STATUS INBOX (UNSEEN)\r\n");
-    if (write(sock, message, strlen(message)) < 0)
-    {
-        printf("Unable to send message.");
-        return -1;
+        char *notify_title = (char *)calloc(25, sizeof(char *));
+        char *notify_body = (char *)calloc(150, sizeof(char *));
+        sprintf(notify_title, "Status for %s", user_address);
+        sprintf(notify_body, "Emails unread %d", unseen);
+        g_notify(g_app, notify_title, notify_body);
+
+        regfree(&r);
+        sleep(60);
     }
 
-    int packet_length = recv(sock, buffer, 512, 0);
-    buffer[packet_length] = '\0';
-
-    // printf("%s\n", buffer);
-
-    regex_t r;
-    const char *regex_text = ".*\\(UNSEEN ([[:digit:]]+)\\).*";
-    int status = regcomp(&r, regex_text, REG_EXTENDED | REG_NEWLINE);
-    if (status != 0)
-    {
-        printf("Regex error compiling '%s'\n", regex_text);
-        return 1;
-    }
-
-    const int unseen = atoi(first_match(&r, buffer));
-
-    printf("Unseen: %d\n", unseen);
-
-    regfree(&r);
     close(sock);
-
-    g_app = g_create("it.davidepucci.PaperBoy");
-    g_run(g_app);
-    sleep(10);
     g_shutdown(g_app);
 
     return 0;
