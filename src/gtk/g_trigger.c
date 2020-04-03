927f92051ab13272f851ecb881b0061e66c9a4c0
streambinder <posta@davidepucci.it>
2020-04-03 12:50:38 +0200

    src: gtk: g_trigger: cleanup

--- src/gtk/g_trigger.c
+++ src/gtk/g_trigger.c
@@ -1,17 +1,25 @@
 #include <gio/gio.h>
 #include <unistd.h>
 
-extern void g_worker_webmail(void *params)
+extern void g_worker_webmail(void *parameter)
 {
-	const char *command = malloc((strlen("xdg-open") + 1 + strlen(params)) * sizeof(char));
-	sprintf(command, "xdg-open %s", params);
+	const char *callback = (char *)g_variant_get_string((GVariant *)parameter, NULL);
+	const char *command = malloc((strlen("xdg-open") + 1 + strlen(callback)) * sizeof(char));
+
+	if (strlen(callback) == 0) {
+		g_print("[g_worker_webmail] callback malformed\n");
+		return;
+	}
+
+	sprintf(command, "xdg-open %s", callback);
 	g_print("[g_worker_webmail] \"%s\" status: %d\n", command, system(command));
+
 	free(command);
+	free(callback);
 }
 
 extern void g_trigger_webmail(GSimpleAction *action, GVariant *parameter, gpointer window)
 {
-	const gchar *value = g_variant_get_string(parameter, NULL);
-	g_print("[g_trigger_webmail] triggered: %s\n", value);
-	g_thread_run(g_worker_webmail, value);
+	g_print("[g_trigger_webmail] triggered\n");
+	g_thread_run(g_worker_webmail, parameter);
 }
