a7376ff20bbf60dd736511def3c62e32024180ec
streambinder <posta@davidepucci.it>
2020-04-04 11:54:11 +0200

    src: gtk: g_trigger: drop const from mutable command variable

--- src/gtk/g_trigger.c
+++ src/gtk/g_trigger.c
@@ -1,10 +1,12 @@
 #include <gio/gio.h>
+#include <stdio.h>
 #include <unistd.h>
 
 extern void g_trigger_webmail(GSimpleAction *action, GVariant *parameter, gpointer window)
 {
 	const char *callback = (char *)g_variant_get_string((GVariant *)parameter, NULL);
-	const char *command = malloc((strlen("xdg-open") + 1 + strlen(callback)) * sizeof(char));
+	char *command = malloc((strlen("xdg-open") + 1 + strlen(callback)) * sizeof(char));
 	sprintf(command, "xdg-open %s", callback);
 	g_print("[g_trigger_webmail] \"%s\" status: %d\n", command, system(command));
+	free(command);
 }
